87c608ef130a5d172e87afecc29d7180426749c9
neketaaa <65709911+neketaaa@users.noreply.github.com>
2022-12-30 00:47:44 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,38 +1,66 @@
 from html.parser import HTMLParser
 import json
-from abc import ABC
+from abc import ABC, abstractmethod
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
 
 
 class IParser(ABC):
-    pass
+
+    @abstractmethod
+    def page_to_html(self, link):
+        pass
 
 
-class ITeamsParser(IParser):
-    pass
+class ITeamsParser(IParser, ABC):
+
+    @abstractmethod
+    def parse_teams(self):
+        pass
 
 
-class IGamesParser(IParser):
-    pass
+class IGamesParser(IParser, ABC):
+    @abstractmethod
+    def parse_games(self):
+        pass
 
 
-class IPlayersParser(IParser):
-    pass
+class IPlayersParser(IParser, ABC):
+
+    @abstractmethod
+    def parse_players(self):
+        pass
 
 
-class UPLParser:
+class IParseFactory(ABC):
+
+    @abstractmethod
+    def parse_teams(self):
+        pass
+
+    @abstractmethod
+    def parse_games(self):
+        pass
+
+    @abstractmethod
+    def parse_players(self):
+        pass
+
+class UPL_Parser(IParser):
 
     def __init__(self, link):
         self.page = self.page_to_html(link)
 
     def page_to_html(self, link):
-        temp_page = requests.get(link)
-        return BeautifulSoup(temp_page.content, 'html.parser')
+        try:
+            temp_page = requests.get(link)
+            return BeautifulSoup(temp_page.content, 'html.parser')
+        except Exception as e: e
 
 
-class TeamParser(UPLParser):
+class UPL_TeamParser(ITeamsParser, UPL_Parser):
 
     def __init__(self, link):
         super().__init__(link)
@@ -41,42 +69,29 @@ class TeamParser(UPLParser):
         result = {
             'teams': []
         }
-        team_list = self.page.find('table', class_='main-tournament-table')
-        items = team_list.find_all('tr')
-        for item in items:
+        # team_list = self.page.find('table', class_='main-tournament-table')
+        # items = team_list.find_all('tr')
+        for item in self.page.find('table', class_='main-tournament-table').find_all('tr'):
             try:
-                team_name = item.find('td', class_='team').find('a').text
-                print(team_name)
-                num = item.find('td', class_='num').text
-                print(num)
-                games = item.find('td', class_='games').text
-                win = item.find('td', class_='win').text
-                draw = item.find('td', class_='draw').text
-                lose = item.find('td', class_='lose').text
-                goal = item.find('td', class_='goal').text
-                miss = item.find('td', class_='miss').text
-                diff = item.find('td', class_='diff').text
-                score = item.find('td', class_='score').text
                 result['teams'].append(
                     {
-                    'team_name': team_name,
-                    'games': games,
-                    'win': win,
-                    'draw': draw,
-                    'lose': lose,
-                    'goal': goal,
-                    'miss': miss,
-                    'diff': diff,
-                    'score': score
+                    'team_name': item.find('td', class_='team').find('a').text,
+                    'num': item.find('td', class_='num').text,
+                    'games': item.find('td', class_='games').text,
+                    'win': item.find('td', class_='win').text,
+                    'draw': item.find('td', class_='draw').text,
+                    'lose': item.find('td', class_='lose').text,
+                    'goal': item.find('td', class_='goal').text,
+                    'miss': item.find('td', class_='miss').text,
+                    'diff': item.find('td', class_='diff').text,
+                    'score': item.find('td', class_='score').text
                     })
-
-
             except AttributeError:
                 pass
         return result
 
 
-class GameParser(UPLParser):
+class UPL_GameParser(IGamesParser, UPL_Parser):
 
     def __init__(self, link):
         super().__init__(link)
@@ -85,16 +100,8 @@ class GameParser(UPLParser):
         result = {
             'games': []
         }
-        games_list = self.page.find('div', class_='table-block')
-        games = games_list.find_all('div', class_='match')
-        for game in games:
+        for game in self.page.find('div', class_='table-block').find_all('div', class_='match'):
             try:
-                time = game.find('td', class_='time').find('a').text
-                first_team = game.find('td', class_='left-team').find('a').text
-                second_team = game.find('td', class_='right-team').find('a').text
-                date = game.find_previous('p', class_='match-date').text
-                num_of_tour = game.find_previous('div', class_='match-name').find('h4').text
-                num_of_tour = re.findall(r'\d+', num_of_tour)[0]
                 try:
                     score = game.find('td', class_='score ended').find('a').text
                 except AttributeError:
@@ -102,11 +109,11 @@ class GameParser(UPLParser):
 
                 result['games'].append(
                     {
-                    'time': time,
-                    'first_team': first_team,
-                    'second_team': second_team,
-                    'date': date,
-                    'num_of_tour': num_of_tour,
+                    'time': game.find('td', class_='time').find('a').text,
+                    'first_team': game.find('td', class_='left-team').find('a').text,
+                    'second_team': game.find('td', class_='right-team').find('a').text,
+                    'date': game.find_previous('p', class_='match-date').text,
+                    'num_of_tour': game.find_previous('div', class_='match-name').find('h4').text,
                     'score': score
                     })
 
@@ -115,43 +122,43 @@ class GameParser(UPLParser):
         return result
 
 
-class PlayerParser(UPLParser):
+class UPL_PlayerParser(IPlayersParser, UPL_Parser):
 
-    def __init__(self, link):
-        super().__init__(link)
+    def __init__(self, squad_file, players_links_file):
+        super().__init__(None)
+        self.squad_file = squad_file
+        self.players_links_file = players_links_file
 
-    def parse_player_link(self, link):
+    def parse_player_link(self):#
         players_links = []
-        page = self.page_to_html(link)
-        all_links = page.find_all('a', class_='mat-list-team fw-500')
-        for one_link in all_links:
-            if ('https://www.ua-football.com'+one_link['href']) not in players_links:
-                players_links.append('https://www.ua-football.com'+one_link['href'])
-        print(players_links)
-        with open('players_links.txt', 'a') as file:
+        with open(self.squad_file, 'r') as f:
+            for line in f:
+                #players_links = []
+                page = self.page_to_html(line)
+                for one_link in page.find_all('a', class_='mat-list-team fw-500'):
+                    if ('https://www.ua-football.com' + one_link['href']) not in players_links:
+                        players_links.append('https://www.ua-football.com' + one_link['href'])
+                print(players_links)
+        with open(self.players_links_file, 'w') as file:
+            #file.write('\n')
             file.write('\n'.join(players_links))
-            # for item in players_links:
-            #     file.write(item)
 
-        #print('https://www.ua-football.com/ua/'+page.find('a', class_='mat-list-team fw-500')['href'])
+    def parse_single_player(self, page):
 
-
-
-    def parse_player(self):
         result = dict()
         keys = ['full_name', 'team_name', 'nationality', 'position', 'age', 'height', 'games_played', 'minutes_played',
                 'games_from_start', 'subtitles', 'games_missed', 'goals', 'assists', 'positive_actions', 'yellow_cards',
                 'red_cards']
-        championship_name = 'Чемпионат Украины'
+        championship_name = 'Общие показатели'
         try:
             result = {
-                'full_name': self.page.find('h1', class_='liga-header__title').text.strip(),
+                'full_name': page.find('h1', class_='liga-header__title').text.strip(),
             }
             for i, value in enumerate(
-                    self.page.find('div', {'class': 'team-about-header'}).find('div').find_all('alAb-dop-item'), 1
+                    page.find('div', {'class': 'team-about-header'}).find('div').find_all('alAb-dop-item'), 1
             ):
                 result[keys[i]] = value.strip()
-            table = self.page.find('table')
+            table = page.find('table')
             champ_row = None
             for row in table.find('tbody').find_all('tr'):
                 if row.find('td').text.strip() == championship_name:
@@ -168,32 +175,82 @@ class PlayerParser(UPLParser):
 
         return result
 
+    def parse_players(self):
+        result = {'players': []}
+        self.parse_player_link()
+        with open(self.players_links_file, 'r') as f:
+            for line in f:
+                result['players'].append(self.parse_single_player(self.page_to_html(line)))
+        return result
+
+class UPLParseFactory(IParseFactory):
+
+    def __init__(self, teams_link, games_link, squad_file, players_links_file):
+        self.teams_link = teams_link
+        self.games_link = games_link
+        self.squad_file = squad_file
+        self.players_links_file = players_links_file
+
+    @property
+    def teams_link(self):
+        return self.__teams_link
+
+    @teams_link.setter
+    def teams_link(self, link):
+        if isinstance(link, str) and 'https://' in link:
+            self.__teams_link = link
+
+    @property
+    def games_link(self):
+        return self.__games_link
+
+    @games_link.setter
+    def games_link(self, link):
+        if isinstance(link, str) and 'https://' in link:
+            self.__games_link = link
+
+    @property
+    def squad_file(self):
+        return self.__squad_file
+
+    @squad_file.setter
+    def squad_file(self, squad_file):
+        if not os.path.isfile(squad_file):
+            raise FileExistsError
+        self.__squad_file = squad_file
+
+    @property
+    def players_links_file(self):
+        return self.__players_links_file
+
+    @players_links_file.setter
+    def players_links_file(self, players_links_file):
+        if not os.path.isfile(players_links_file):
+            raise FileExistsError
+        self.__players_links_file = players_links_file
+
+
+    def parse_games(self):
+        return UPL_GameParser(self.games_link).parse_games()
+
+    def parse_teams(self):
+        return UPL_TeamParser(self.teams_link).parse_teams()
+
+    def parse_players(self):
+        return UPL_PlayerParser(self.squad_file, self.players_links_file).parse_players()
 
 
 
 
-teams = TeamParser('https://football.ua/ukraine/table.html')
-teams_dict= teams.parse_teams()
-example1 = GameParser('https://football.ua/ukraine/results/761/')
-print(example1.parse_games())
-example2 = PlayerParser('https://www.ua-football.com/ua/stats/player/65408-mihail-mudrikua')
-example2.parse_player()
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/1533-sk-dnipro-1')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/2-shahtar-doneck')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/1-dinamo-kiev')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/18-zorya-lugansk')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/5-fk-oleksandriya')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/1254-kolos-kovalevka')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/1504-metalist-1925-harkov')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/352-krivbas-krivoy-rog')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/4-vorskla-poltava')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/1230-veres-rovno')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/6200-metalist-harkov')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/1410-ruh-lvov')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/1619-fk-minay')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/1253-ingulec-petrovo')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/14-chornomorec-odessa')
-example2.parse_player_link('https://www.ua-football.com/ua/stats/team/312-fk-lviv')
+teams_links = 'https://football.ua/ukraine/table.html'
+games_link = 'https://football.ua/ukraine/results/761/'
+upl = UPLParseFactory(teams_links, games_link, 'upl_squads.txt', 'players_links.txt')
+print(upl.parse_teams())
+print(upl.parse_games())
+print(upl.parse_players())
+
+
+
 
 
 
