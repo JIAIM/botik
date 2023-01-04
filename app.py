9fd98e308bb434f0feba18540505a161e9fc456c
JIAIM <50765314+JIAIM@users.noreply.github.com>
2023-01-04 17:34:31 +0200

    update introduce

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request
 import requests
 from dotenv import load_dotenv
@@ -12,18 +13,34 @@ def get_from_env(key):
     load_dotenv(dotenv_path)
     return os.environ.get(key)
 
-def send_message(chat_id, text):
+league_buttons = ["Ukraine", "England"]
+
+def send_message(chat_id, text, buttons=None):
     method = "sendMessage"
     token = get_from_env("TELEGRAM_BOT_TOKEN")
     url = f"https://api.telegram.org/bot{token}/{method}"
     data = {"chat_id": chat_id, "text": text}
-    requests.post(url, data=data)
 
+    if buttons:
+        keyboard = [[button] for button in buttons]
+        print(keyboard)
+        reply_markup = {"keyboard": keyboard}
+        print(reply_markup)
+        data["reply_markup"] = reply_markup
+
+    requests.post(url, data=data)
 @app.route('/', methods=["POST"])  # localhost:5000 -> telegram`s message
 def process():
     chat_id = request.json["message"]["chat"]["id"]
-    user_name = request.json['message']['from']['first_name']
-    send_message(chat_id=chat_id, text=f"Hi {user_name}! Welcome to football bot!\nChoose league:")
+    user_name = request.json["message"]["from"]["first_name"]
+    send_message(chat_id=chat_id, text=f"Hi {user_name}! Welcome to football bot!\nChoose league:", buttons=league_buttons)
+    text = request.json["message"]["text"]
+    if text == "Ukraine":
+        send_message(chat_id=chat_id, text="nash")
+    elif text == "England":
+        send_message(chat_id=chat_id, text="ne nash")
+    else:
+        send_message(chat_id=chat_id, text=f"Hi {user_name}! Welcome to football bot!\nChoose league:", buttons=league_buttons)
     print(request.json)
     return {"ok": True}
 
